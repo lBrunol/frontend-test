cfbc914bf8a9f0ebf4167b0f92dcf652ff15e88b
gabriel yago <gabriellok1212@gmail.com>
2020-06-11 19:08:20 -0300

    teste finalizado

---
+++ main.js
@@ -0,0 +1,27 @@
+function abrirmodal1() {
+  document.getElementById('modal1').style.display = 'block';
+}
+function fecharmodal1() {
+  document.getElementById('modal1').style.display = 'none';
+}
+
+function abrirmodal2() {
+  document.getElementById('modal2').style.display = 'block';
+}
+function fecharmodal2() {
+  document.getElementById('modal2').style.display = 'none';
+}
+
+function abrirmodal3() {
+  document.getElementById('modal3').style.display = 'block';
+}
+function fecharmodal3() {
+  document.getElementById('modal3').style.display = 'none';
+}
+
+function abrirmodal4() {
+  document.getElementById('modal4').style.display = 'block';
+}
+function fecharmodal4() {
+  document.getElementById('modal4').style.display = 'none';
+}
